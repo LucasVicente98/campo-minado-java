e570d1ba3a90084b019a75a75abb5333e932abf8
Lucas Leite Vicente <lucasleitev98@gmail.com>
2023-01-31 16:38:26 -0300

    v0

---
+++ src/br/com/vicente027/cm/modelo/CampoEvento.java
@@ -0,0 +1,6 @@
+package br.com.vicente027.cm.modelo;
+
+public enum CampoEvento {
+
+	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
+}
---
+++ src/br/com/vicente027/cm/modelo/Tabuleiro.java
@@ -0,0 +1,112 @@
+package br.com.vicente027.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Tabuleiro implements CampoObservador {
+
+	private final int linhas;
+	private final int colunas;
+	private final int minas;
+
+	private final List<Campo> campos = new ArrayList<>();
+	private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		// Inicializando o tabuleiro através do construtor
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	public void paraCada(Consumer<Campo> funcao) {
+		campos.forEach(funcao);
+	}
+
+	public void registrarObservador(Consumer<ResultadoEvento> observador) {
+		observadores.add(observador);
+	}
+
+	private void notificarObservadores(Boolean resultado) {
+		observadores.stream().forEach(o -> o.accept(new ResultadoEvento(resultado)));
+	}
+
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst()
+				.ifPresent(c -> c.abrir());
+	}
+
+	public void alternarMarcacao(int linha, int coluna) {
+		campos.stream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst()
+				.ifPresent(c -> c.alternarMarcacao());
+	}
+
+	private void gerarCampos() {
+		for (int linhaT = 0; linhaT < linhas; linhaT++) {
+			for (int colunaT = 0; colunaT < colunas; colunaT++) {
+				Campo campo = new Campo(linhaT, colunaT);
+				campo.registrarObservador(this);
+				campos.add(campo);
+			}
+		}
+	}
+
+	// Percorre a lista 2x e faz a tentativa de associamento dos vizinhos
+	// Define qm pode ser vizinho de acordo com a proximidade
+	private void associarVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+
+		do {
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
+		} while (minasArmadas < minas);
+	}
+
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		if (evento == CampoEvento.EXPLODIR) {
+			mostrarMinas();
+			notificarObservadores(false);
+		} else if (objetivoAlcancado()) {
+			notificarObservadores(true);
+		}
+	}
+
+	private void mostrarMinas() {
+		campos.stream().filter(c -> c.isMinado()) // Só mostra os campos que tem minas
+				.filter(c -> !c.isMarcado()).forEach(c -> c.setAberto(true));
+	}
+}
